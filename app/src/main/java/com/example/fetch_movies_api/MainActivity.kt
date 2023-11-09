b34559e075f752cfaa5987b5cd26e674e983333d
Mohammed-Fares-Dev <mohammedfarestech@gmail.com>
2023-11-09 21:49:46 +0100

    implement main activity

--- app/src/main/java/com/example/fetch_movies_api/MainActivity.kt
+++ app/src/main/java/com/example/fetch_movies_api/MainActivity.kt
@@ -1,11 +1,15 @@
 package com.example.fetch_movies_api
 
-import androidx.appcompat.app.AppCompatActivity
+
 import android.os.Bundle
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        supportFragmentManager.beginTransaction().replace(R.id.frg1, MoviesList()).commit()
     }
 }
