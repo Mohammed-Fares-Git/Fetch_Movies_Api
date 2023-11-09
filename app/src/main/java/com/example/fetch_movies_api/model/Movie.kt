a069e9849b5ecf13b0ca5a54ae2be69996d49137
Mohammed-Fares-Dev <mohammedfarestech@gmail.com>
2023-11-09 21:01:10 +0100

    create movie model

---
+++ app/src/main/java/com/example/fetch_movies_api/model/Movie.kt
@@ -0,0 +1,12 @@
+package com.example.fetch_movies_api.model
+
+import java.io.Serializable
+
+
+class Film : Serializable {
+    var traduction: Boolean? = null
+    var image: String? = null
+    var price: Int? = null
+    var name: String? = null
+    var id: String? = null
+}
