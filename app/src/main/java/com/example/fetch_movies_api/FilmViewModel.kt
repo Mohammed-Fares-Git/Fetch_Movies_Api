93123606e363e12174e29df5f0fa197249cdb0e8
Mohammed-Fares-Dev <mohammedfarestech@gmail.com>
2023-11-09 21:55:26 +0100

    implement shared view model

--- app/src/main/java/com/example/fetch_movies_api/FilmViewModel.kt
+++ app/src/main/java/com/example/fetch_movies_api/FilmViewModel.kt
@@ -1,5 +1,18 @@
 package com.example.fetch_movies_api
 
-class FilmViewModel {
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.example.fetch_movies_api.model.Film
 
+
+class FilmViewModel : ViewModel() {
+    private val filmMutableLiveData = MutableLiveData<Film>()
+    fun getFilmMutableLiveData(): LiveData<Film> {
+        return filmMutableLiveData
+    }
+
+    fun setFilmMutableLiveData(film: Film) {
+        filmMutableLiveData.value = film
+    }
 }
