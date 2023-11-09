2da15f80ac70a70df896c98b4a6f3f3b7f9f0918
Mohammed-Fares-Dev <mohammedfarestech@gmail.com>
2023-11-09 21:53:10 +0100

    implement second fragment

--- app/src/main/java/com/example/fetch_movies_api/MovieDetails.kt
+++ app/src/main/java/com/example/fetch_movies_api/MovieDetails.kt
@@ -1,25 +1,36 @@
 package com.example.fetch_movies_api
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.fradmentstutos.databinding.FragmentFrg2Binding
 
 
-class MovieDetails : Fragment() {
-    private var param1: String? = null
-    private var param2: String? = null
-
-
+class Frg2 : Fragment() {
+    private var frg2Binding: FragmentFrg2Binding? = null
+    private val sharedeViewModel: FilmViewModel? = null
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_movie_details, container, false)
+        frg2Binding = FragmentFrg2Binding.inflate(inflater, container, false)
+        /* sharedeViewModel = new ViewModelProvider(requireActivity()).get(CarOperatingCostCalculatorViewModel.class);
+
+        sharedeViewModel.getCarOperatingCostMutableLiveData().observe(getViewLifecycleOwner(), new Observer<CarOperatingCost>() {
+            @Override
+            public void onChanged(CarOperatingCost carOperatingCost) {
+                frg2Binding.tvKilometersPerYear.setText(String.valueOf(carOperatingCost.getKilometersPerYear()));
+                frg2Binding.tvEngineDisplacement.setText(String.valueOf(carOperatingCost.getEngineDisplacement()));
+                frg2Binding.tvFuelType.setText(carOperatingCost.getFuelType());
+                fr g2Binding.tvResult.setText("Monthly operating cost: " +String.format("%.2f", carOperatingCost.calculateMonthlyCost())+" euros");
+            }
+        });
+*/return frg2Binding.getRoot()
     }
-
-
-}
+}}
